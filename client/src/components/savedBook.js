cd4c4ac96752f5c9e59f8e0440e314c7978a90f9
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-06-26 12:15:01 -0400

    seeded data loads on saved page, disappears when refreshed

--- client/src/components/savedBook.js
+++ client/src/components/savedBook.js
@@ -1,26 +1,8 @@
 import React from 'react';
 
-async function getBooks() {
-  let res;
-  try {
-    res = await fetch("/api/books");
-    console.log('res');
-    console.log(res);
-  } catch (err) {
-    console.log('err.message');
-    console.log(err.message);
-  }
-  console.log('past catch');
-  const json = JSON.stringify(res);
-  // const json = res.json();
-  console.log('json');
-  console.log(json);
 
-  return json;
-}
 
 function savedBook({title, authors, description, image, link}) {
-  getBooks();
 
   return (
     <div className='bookdiv'>
--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -2,52 +2,41 @@ import React from 'react';
 import SavedBook from '../components/savedBook';
 // import APICAll from '../routes/apiCalls';
 // import API from '../api';
+let books;
 
+getBooks();
 
-// async function hello() {
-//   // const htmlRoutes = {
-//     // async getBooks() {
-//         const res = await fetch(`/api/books`);
-//         const json = await res.json();
-//         console.log('json');
-//         console.log(json);
-//         return json;
-//     // }
-// // }
-// }
+async function getBooks() {
+  
+  try {
+    books = await fetch("/api/books");
+  } catch (err) {
+    console.log('err.message');
+    console.log(err.message);
+  }
+  // console.log('past catch');
+  books = await books.json();
+  console.log('res');
+  console.log(books);
 
-// fetch("/api/books", {
-//   method: "GET",
-//   body: JSON.stringify(),
-//   headers: {
-//     Accept: "application/json, text/plain, */*",
-//     "Content-Type": "application/json"
-//   },
-// })
-// .then(response => {
-//   console.log('response');
-//   console.log(response);
-//   return response.json();
-// })
-// .then(data => {
-//   console.log('data');
-//   console.log(data);
-// })
-// .catch(err => {
-//   console.log(err.message);
-// });
-
-
+  return books;
+}
 
 function Saved() {
-  // API.getBooks();
+  // console.log('getBooks');
+  // console.log(getBooks());
+
+  console.log('books');
+  console.log(books);
 
   return (
     <div className='page'>
         <h2>Saved</h2>
         <div className='books'>
-            <SavedBook title={'title1'} authors={['authors1']} description={'description1'} image={'Image unavailable'} link={'link1'}/>
-            <SavedBook title={'title2'} authors={['authors2']} description={'description2'} image={'Image unavailable'} link={'link2'}/>
+            {books.map(book =>
+              <SavedBook title={book.title} authors={book.authors} description={book.description} image={book.image} link={book.image}/>
+            )}
+        {/* <SavedBook title={books[0].title} authors={books[0].authors} description={books[0].description} image={books[0].image} link={books[0].image}/>         */}
         </div>
         
     </div>
